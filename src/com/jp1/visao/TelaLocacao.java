dd2162fec25dc70595a7e611eb4bfd72819253e5
Wenderson <geydson87@gmail.com>
2022-12-10 18:05:41 -0300

    Commit 10/12
    Co-Authored-By: Alexandre <114264855+Alexandre320291@users.noreply.github.com>
    Co-Authored-By: Andre-Luiz01 <105212889+Andre-Luiz01@users.noreply.github.com>

--- src/com/jp1/visao/TelaLocacao.java
+++ src/com/jp1/visao/TelaLocacao.java
@@ -26,15 +26,17 @@ public class TelaLocacao extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        setBorder(null);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGap(0, 406, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGap(0, 286, Short.MAX_VALUE)
         );
 
         pack();
